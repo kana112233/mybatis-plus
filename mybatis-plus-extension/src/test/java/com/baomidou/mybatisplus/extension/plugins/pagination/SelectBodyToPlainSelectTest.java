313b90af5c8e002d8f85eb75c68171ebad055267
makeyy22112 <makey221112@gmail.com>
2019-07-31 10:49:35 +0800

    SelectBody强转PlainSelect不支持sql里面带union 临时解决方法

--- mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/plugins/pagination/SelectBodyToPlainSelectTest.java
+++ mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/plugins/pagination/SelectBodyToPlainSelectTest.java
@@ -9,14 +9,16 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- *  SelectBody强转PlainSelect不支持sql里面带union
+ * SelectBody强转PlainSelect不支持sql里面带union
+ * 临时解决方法
  * @date 2019/7/31
  */
 class SelectBodyToPlainSelectTest {
 
     @Test
-    void testSelectBodyToPlainSelectSuccess(){
+    void testSelectBodyToPlainSelectSuccess() {
         String originalSql = "select * from test";
+
         Select selectStatement = null;
         try {
             selectStatement = (Select) CCJSqlParserUtil.parse(originalSql);
@@ -30,8 +32,9 @@ class SelectBodyToPlainSelectTest {
     }
 
     @Test
-    void testSelectBodyToPlainSelectThrowException(){
+    void testSelectBodyToPlainSelectThrowException() {
         String originalSql = "select * from test union select * from test";
+
         Select selectStatement = null;
         try {
             selectStatement = (Select) CCJSqlParserUtil.parse(originalSql);
@@ -41,9 +44,32 @@ class SelectBodyToPlainSelectTest {
         assert selectStatement != null;
         try {
             PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
-            System.out.println("don't hit this"+plainSelect);
+            System.out.println("don't hit this" + plainSelect);
         } catch (Exception e) {
             assertThat(e.getMessage()).isEqualTo("net.sf.jsqlparser.statement.select.SetOperationList cannot be cast to net.sf.jsqlparser.statement.select.PlainSelect");
+        }
+    }
+
+    /**
+     * 临时解决方法
+     */
+    @Test
+    void testResoleSelectBodyToPlainSelectThrowException() {
+        String originalSql = "select * from (select * from test union select * from test) as temp";
+
+        Select selectStatement = null;
+        try {
+            selectStatement = (Select) CCJSqlParserUtil.parse(originalSql);
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+        }
+        assert selectStatement != null;
+
+        try {
+            PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
+            assert plainSelect != null;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
