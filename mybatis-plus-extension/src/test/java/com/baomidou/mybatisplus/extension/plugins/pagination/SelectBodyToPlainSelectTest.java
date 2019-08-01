f115ae7b9aabd7b1a2dd2d8f3168bb30ba533a56
makeyy22112 <makey221112@gmail.com>
2019-08-01 16:18:00 +0800

    SelectBody强转PlainSelect不支持sql里面带union 加入SetOperationList 处理union语句

--- mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/plugins/pagination/SelectBodyToPlainSelectTest.java
+++ mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/extension/plugins/pagination/SelectBodyToPlainSelectTest.java
@@ -1,24 +1,35 @@
 package com.baomidou.mybatisplus.extension.plugins.pagination;
 
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SetOperationList;
+import net.sf.jsqlparser.statement.select.WithItem;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * SelectBody强转PlainSelect不支持sql里面最外层带union
  * 临时解决方法
- * @date 2019/7/31
+ * 提交pr解决
  */
 class SelectBodyToPlainSelectTest {
 
+    private String originalSql = "select * from test";
+    private String actualSql = "SELECT * FROM test";
+
+    private String originalUnionSql = "select * from test union select * from test2";
+    private String actualUnionSql = "SELECT * FROM test UNION SELECT * FROM test2";
+
     @Test
     void testSelectBodyToPlainSelectSuccess() {
-        String originalSql = "select * from test";
-
         Select selectStatement = null;
         try {
             selectStatement = (Select) CCJSqlParserUtil.parse(originalSql);
@@ -28,16 +39,15 @@ class SelectBodyToPlainSelectTest {
         assert selectStatement != null;
 
         PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
-        assert plainSelect != null;
+        assertThat(plainSelect.toString())
+            .isEqualTo(actualSql);
     }
 
     @Test
     void testSelectBodyToPlainSelectThrowException() {
-        String originalSql = "select * from test union select * from test";
-
         Select selectStatement = null;
         try {
-            selectStatement = (Select) CCJSqlParserUtil.parse(originalSql);
+            selectStatement = (Select) CCJSqlParserUtil.parse(originalUnionSql);
         } catch (JSQLParserException e) {
             e.printStackTrace();
         }
@@ -67,10 +77,88 @@ class SelectBodyToPlainSelectTest {
 
         try {
             PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
-            assert plainSelect != null;
+            assertThat(plainSelect.toString())
+                .isEqualTo("SELECT * FROM (SELECT * FROM test UNION SELECT * FROM test) AS temp");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * 最终解决方法
+     */
+    @Test
+    void testSetOperationList() {
+        parseSql(originalSql, actualSql);
+
+        parseSql(originalUnionSql,
+            actualUnionSql);
+
+
+    }
+
+    @Test
+    void testWithItemNotSupportWith() {
+        String originalSql = "with cc as (select * from test) select * from cc";
+        parseSql(originalSql, "SELECT * FROM cc");
+    }
+
+
+    private void parseSql(String originalSql, String actualStr) {
+        Select selectStatement = null;
+        try {
+            selectStatement = (Select) CCJSqlParserUtil.parse(originalSql);
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+        }
+        assert selectStatement != null;
+
+        if (selectStatement.getSelectBody() instanceof PlainSelect) {
+            PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
+            assertThat(plainSelect.toString())
+                .isEqualTo(actualStr);
+        } else if (selectStatement.getSelectBody() instanceof SetOperationList) {
+            SetOperationList setOperationList = (SetOperationList) selectStatement.getSelectBody();
+            assertThat(setOperationList.toString())
+                .isEqualTo(actualStr);
+        } else if (selectStatement.getSelectBody() instanceof WithItem) {
+            System.out.println("WithItem");
+            assert false;
+        } else {
+            System.out.println("else");
+            assert false;
+        }
+    }
+
+    @Test
+    void testPaginationInterceptorConcatOrderByBefore() {
+        Page<?> page = new Page<>();
+        List<OrderItem> orderItems = new ArrayList<>();
+        OrderItem order = new OrderItem();
+        order.setAsc(true);
+        order.setColumn("column");
+        orderItems.add(order);
+        page.setOrders(orderItems);
+        String result = PaginationInterceptor
+            .concatOrderBy(originalSql, page);
+
+        assertThat(result).isEqualTo(actualSql);
+    }
+
+    @Test
+    void testPaginationInterceptorConcatOrderByFix() {
+        Page<?> page = new Page<>();
+        List<OrderItem> orderItems = new ArrayList<>();
+        OrderItem order = new OrderItem();
+        order.setAsc(true);
+        order.setColumn("column");
+        orderItems.add(order);
+        page.setOrders(orderItems);
+        String result = PaginationInterceptor
+            .concatOrderBy(originalUnionSql, page);
+
+        assertThat(result).isEqualTo(actualUnionSql);
+    }
+
 
 }
